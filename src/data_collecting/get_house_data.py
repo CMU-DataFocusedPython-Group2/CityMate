9dfeae5cd9f1c3f70ba5fc51784ab359bf9da8f6
Yixuan Guo <2761683494@qq.com>
2020-10-11 00:26:24 +0800

    Update the file path to correct relative path.

--- src/data_collecting/get_house_data.py
+++ src/data_collecting/get_house_data.py
@@ -132,7 +132,7 @@ def update_house_data():
 
     print(len(urls))
 
-    with open("sites.txt", "w") as output_file:
+    with open("../../data/sites.txt", "w") as output_file:
         for i in urls:
             output_file.write(i)
             output_file.write("\n")
@@ -141,7 +141,7 @@ def update_house_data():
     # wait until writing the url data file is done.
     time.sleep(1)
 
-    with open("sites.txt", "r") as input_file:
+    with open("../../data/sites.txt", "r") as input_file:
         original = input_file.readlines()
         urls = list()
         for i in original:
@@ -173,12 +173,12 @@ def update_house_data():
         df.loc[i] = get_house_info(house_html)
         i += 1
 
-    df.to_excel("house_info.xlsx", encoding='utf-8')
+    df.to_excel("../../data/house_info.xlsx", encoding='utf-8')
 
     # data cleaning
     new_df = pd.DataFrame(df)
     new_df = new_df.loc[new_df['name'] != "null"]
-    new_df.to_excel("clean_house_data.xlsx", encoding='utf-8')
+    new_df.to_excel("../../data/clean_house_data.xlsx", encoding='utf-8')
 
 
 if __name__ == "__main__":
